b19fec81d173a7cbef7c5d9af9b03f9d83572c9d
Hsu Zhong Jun <27919917+dcshzj@users.noreply.github.com>
2024-06-26 00:30:02 +0800

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -20,7 +20,6 @@ export default function robots(): MetadataRoute.Robots {
       // @ts-expect-error blah
       siteMap: sitemap,
       navBarItems: navbar,
-      // @ts-expect-error blah
       footerItems: footer,
       lastUpdated,
     },
