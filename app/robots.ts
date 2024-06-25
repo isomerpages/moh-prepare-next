bf81bf4c3e089de9c59721748ace8b16c03c380e
Hsu Zhong Jun <27919917+dcshzj@users.noreply.github.com>
2024-06-26 00:27:20 +0800

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -1,6 +1,7 @@
 import config from "@/data/config.json";
 import footer from "@/data/footer.json";
 import navbar from "@/data/navbar.json";
+import sitemap from "@/sitemap.json";
 import { getRobotsTxt } from "@opengovsg/isomer-components";
 import type { MetadataRoute } from "next";
 
@@ -16,6 +17,8 @@ export default function robots(): MetadataRoute.Robots {
   return getRobotsTxt({
     site: {
       ...config.site,
+      // @ts-expect-error blah
+      siteMap: sitemap,
       navBarItems: navbar,
       // @ts-expect-error blah
       footerItems: footer,
